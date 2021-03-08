73aebfb1392bd8617fe67c139527ca9cd2e78a04
HarveyTsang <13025483658@163.com>
2021-03-08 15:52:05 +0800

    add proj

---
+++ HTVideoEdit/HTVideoEdit/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HTVideoEdit
+//
+//  Created by mac on 2021/3/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HTVideoEdit/HTVideoEdit/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  HTVideoEdit
+//
+//  Created by mac on 2021/3/8.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
